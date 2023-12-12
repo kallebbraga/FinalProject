9b4f29a5f8317ddaa01c461b40fcf537d961867d
kallebbraga <kbraga@caldwell.edu>
2023-12-11 22:11:32 -0500

    yes

--- script.js
+++ script.js
@@ -1,11 +1,10 @@
 const apiKey = '3GSYUIH0HFVSLJGF';
-const stockSymbol = 'GAME'; // GameSquare stock symbol
+const stockSymbol = 'GAME';
 
 const stockPriceElement = document.getElementById('stock-price');
 const updateCounterElement = document.getElementById('update-counter');
 const notificationElement = document.getElementById('notification');
 const yahooButton = document.getElementById('yahoo-button');
-const updateButton = document.getElementById('update-button');
 
 let updateCounter = 0;
 const maxUpdatesPerDay = 25;
@@ -15,7 +14,6 @@ function updateCounterDisplay() {
 }
 
 function fetchStockPrice() {
-    // Check if the maximum number of updates per day is reached
     if (updateCounter >= maxUpdatesPerDay) {
         console.log('Maximum updates per day reached. Stopping updates.');
         return;
@@ -24,28 +22,20 @@ function fetchStockPrice() {
     fetch(`https://www.alphavantage.co/query?function=GLOBAL_QUOTE&symbol=${stockSymbol}&apikey=${apiKey}`)
         .then(response => response.json())
         .then(data => {
-            console.log('API Response:', data);
-
-            // Check if the response contains 'Global Quote' and '05. price'
             const globalQuote = data['Global Quote'];
             if (globalQuote && globalQuote['05. price']) {
                 const stockPrice = parseFloat(globalQuote['05. price']);
                 stockPriceElement.textContent = `Stock Price: $${stockPrice.toFixed(2)}`;
 
-                // Check if the stock price is over $2 or under $2
                 if (stockPrice > 2) {
                     hideNotification();
                 } else {
                     showNotification('Go buy more stock!');
                 }
 
-                // Increment the update counter
                 updateCounter++;
-
-                // Update the counter display
                 updateCounterDisplay();
 
-                // Check if the maximum updates per day is reached after the increment
                 if (updateCounter >= maxUpdatesPerDay) {
                     console.log('Maximum updates per day reached. Stopping updates.');
                 }
@@ -61,28 +51,20 @@ function fetchStockPrice() {
 
 function showNotification(message) {
     notificationElement.textContent = message;
-    notificationElement.style.backgroundColor = '#FF5733'; // orange color
     notificationElement.style.display = 'block';
     setTimeout(() => {
         hideNotification();
-    }, 5000); // Hide after 5 seconds
+    }, 5000);
 }
 
 function hideNotification() {
     notificationElement.style.display = 'none';
 }
 
-// Open Yahoo Finance when the button is clicked
 yahooButton.addEventListener('click', () => {
     window.open('https://finance.yahoo.com/quote/GAME', '_blank');
 });
 
-// Manual update when the button is clicked
-updateButton.addEventListener('click', () => {
-    fetchStockPrice();
-});
-
-// Initial fetch
 fetchStockPrice();
 
 // Fetch stock price every 10 seconds
